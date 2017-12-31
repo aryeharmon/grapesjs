7ab693193c468c8facefba72c62dff8df5ecd6d8
Aryeh Armon <aryeh.armon@gmail.com>
2017-12-31 18:20:56 +0200

    added flex start

--- src/utils/Sorter.js
+++ src/utils/Sorter.js
@@ -971,6 +971,28 @@ module.exports = Backbone.View.extend({
         opts.avoidUpdateStyle = 1;
       }
 
+      // aryeh edit
+      if ($('.gjs-pn-btn.fa.fa-bars.gjs-pn-active').length === 0) {
+        if (modelToDrop.type !== 'region') {
+          if (dst.parentNode.tagName === 'BODY' || dst.className.indexOf('flex-start') === -1) {
+            // modelToDrop = "<div class='flex-start'>" + modelToDrop + "</div>";
+            // modelToDrop = "<div class='flex-start'><div>TEST</div></div>";
+            targetCollection.add("<div class='flex-start'></div>", opts);
+            var targetCollection = $($(dst).children()[opts.at]).data('collection');
+            // targetCollection.add("<div></div>", opt);
+
+            targetCollection.add("<div></div>", opts);
+            var targetCollection = $($($(dst).children()[opts.at]).find('div')).data('collection');
+
+            opts.at++;
+          } else if (dst.className.indexOf('flex-start') > -1) {
+            targetCollection.add("<div></div>", opts);
+            var targetCollection = $($(dst).children()[opts.at]).data('collection');
+          }
+        }
+      }
+      // end aryeh edit
+
       created = targetCollection.add(modelToDrop, opts);
 
       if (!dropContent) {
